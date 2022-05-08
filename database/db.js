0b4d26be5d65e4a0fe272376b37025504cdea283
FelipeLL <f3lip3.3z@hotmail.com>
2022-05-08 15:40:36 -0500

    make it better x2

--- database/db.js
+++ database/db.js
@@ -3,8 +3,10 @@ import { Config } from "../config/index.js";
 
 
 const db = new Sequelize(Config.database, Config.username, Config.password, {
-  host: "localhost",
+  host: "us-cdbr-east-05.cleardb.net",
   dialect: "mysql",
 });
 
 export default db;
+
+
