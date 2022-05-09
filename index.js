33e30584ab3f996088bda71873580bd9c9b75787
FelipeLL <f3lip3.3z@hotmail.com>
2022-05-08 23:36:22 -0500

    se modifico la config de los cors x5

--- index.js
+++ index.js
@@ -20,23 +20,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const corsConfig = {
-  credentials: true,
+  // credentials: true,
   // origin: "https://bespoke-zabaione-6e591a.netlify.app/",
+  origin: "*"
 };
 app.use(cors(corsConfig));
 
-app.use(function (req, res, next) {
 
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  next();
-});
 
 app.use(express.static(path.join(__dirname, "dbimages")))
 
