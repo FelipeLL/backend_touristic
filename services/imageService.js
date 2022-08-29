306bd67d05cdc23d62765205efdf0c0c98421170
FelipeLL <f3lip3.3z@hotmail.com>
2022-08-29 18:58:34 -0500

    se modifico el config aws update para intentar solucionar el error de credenciales con aws

--- services/imageService.js
+++ services/imageService.js
@@ -12,7 +12,7 @@ aws.config.update({
     region: "us-east-1",
     aws_access_key_id: process.env.aws_access_key_id,
     aws_secret_access_key: process.env.aws_secret_access_key,
-    aws_session_token: process.env.aws_session_token
+
 });
 
 export const add = async (file, idEstacion) => {
