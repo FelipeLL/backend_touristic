5ee3352c6c707bc2491ebc294b8729774eece793
FelipeLL <f3lip3.3z@hotmail.com>
2022-10-02 09:56:21 -0500

    Se agrego el Connection Pool

--- database/db.js
+++ database/db.js
@@ -5,6 +5,12 @@ import { Config } from "../config/index.js";
 const db = new Sequelize(Config.database, Config.username, Config.password, {
   host: "localhost",
   dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
 });
 
 export default db;
